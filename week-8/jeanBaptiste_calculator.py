8d4c89542e68c28e6fa6a0592c6b8d4ce7e9c480
Sarah Jean Baptiste <sjeand@hotmail.com>
2021-07-18 19:05:24 -0500

    add calculator

---
+++ week-8/jeanBaptiste_calculator.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def divide(x, y):
+    return x / y
+    
+print(add(5, 8))
+
+print(subtract(10, 2))
+
+print(divide(9, 2))
+
